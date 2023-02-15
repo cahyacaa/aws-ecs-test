329b2b9941b8073f0832845e532d51a642221fce
cahyacaa <cahyanug3@gmail.com>
2023-02-15 15:38:26 +0700

    add basic app

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		log.Fatal("ENV Failed to initialized")
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/testing/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Hi %s!", name),
+		})
+	})
+	err := r.Run(fmt.Sprintf(":%s", port))
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
