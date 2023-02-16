40debbea5ecebe23bc561a3ce97be767fff427d9
cahyacaa <cahyanug3@gmail.com>
2023-02-16 19:59:38 +0700

    update

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	r.GET("/testing/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Hi %s!", name),
+			"message": fmt.Sprintf("Hi %s!, Congratulations", name),
 		})
 	})
 	r.GET("/greeting/:name", func(c *gin.Context) {
